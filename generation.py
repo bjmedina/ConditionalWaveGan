b829fb2bd246b95cf19ee07d0047ec32a61f6565
Bryan Medina <bjmedina.cs@gmail.com>
2019-11-20 06:07:23 -0500

    Added files to edit. TODO: Get wavegan to generate audio samples once it has finished training.

---
+++ generation.py
@@ -0,0 +1,20 @@
+import tensorflow as tf
+from IPython.display import display, Audio
+
+# Load the graph
+tf.reset_default_graph()
+saver = tf.train.import_meta_graph('infer.meta')
+graph = tf.get_default_graph()
+sess = tf.InteractiveSession()
+saver.restore(sess, '/home/me/Documents/sample_code/wavegan/model.ckpt')
+
+# Create 50 random latent vectors z
+_z = (np.random.rand(50, 100) * 2.) - 1
+
+# Synthesize G(z)
+z = graph.get_tensor_by_name('z:0')
+G_z = graph.get_tensor_by_name('G_z:0')
+_G_z = sess.run(G_z, {z: _z})
+
+# Play audio in notebook
+display(Audio(_G_z[0, :, 0], rate=16000))
